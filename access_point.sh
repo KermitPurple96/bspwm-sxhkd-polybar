19008356d9924f738e9029e42207302bb8c400b2
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2022-10-09 22:10:59 +0200

    Update access_point.sh

--- access_point.sh
+++ access_point.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#ADD as ~/.config/bin/access_point.sh
 
 AP=$(/usr/sbin/route | grep default | awk '{print $2}')
 if [[ "$AP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
