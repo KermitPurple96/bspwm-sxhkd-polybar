0b57f78895e8384cecdce6610a70769371297826
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2022-10-09 22:09:41 +0200

    Update ethernet_status.sh

--- ethernet_status.sh
+++ ethernet_status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#ADD AS .config/bin/ethernet_status
+#ADD as ~/.config/bin/ethernet_status.sh
 
 IP=$(/usr/sbin/ifconfig | grep ens33 -A1 | grep inet | awk '{print $2}')
 if [[ "$IP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
