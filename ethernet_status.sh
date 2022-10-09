0d45c0976511bf4f6e7571788bb704376c2e1c78
KermitPurple96 <103221169+KermitPurple96@users.noreply.github.com>
2022-10-09 22:06:00 +0200

    Create ethernet_status.sh

---
+++ ethernet_status.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#ADD AS .config/bin/ethernet_status
+
+IP=$(/usr/sbin/ifconfig | grep ens33 -A1 | grep inet | awk '{print $2}')
+if [[ "$IP" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+  echo "%{F#2495e7} %{F#ffffff}$(/usr/sbin/ifconfig | grep ens33 -A1 | grep inet | awk '{print $2}')%{u-}"
+else 
+  echo "%{F#2495e7} %{F#ffffff}Disconnected"
+fi
